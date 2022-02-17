2e7d319bf0f73d56a57dd53f8f444d10a388ccf7
Jose Manuel Rojas <jose.rojas@conekta.com>
2022-02-17 16:52:20 -0500

    resolving comments

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -2,91 +2,68 @@ import React, { useEffect, useState } from 'react';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
 import { useNavigate } from "react-router-dom";
-// import CssBaseline from '@mui/material/CssBaseline';
 import Box from '@mui/material/Box';
 import Container from '@mui/material/Container';
 import Stack from '@mui/material/Stack';
-// import { AccessAlarm, JoinLeftSharp, ThreeDRotation } from '@mui/icons-material';
 import AccountCircleOutlinedIcon from '@mui/icons-material/AccountCircleOutlined';
 import './styles.scss';
 
 
 const Login = () => {
   const navigate = useNavigate();
-
   const [isLogin, setIsLogin] =useState(false);
   const [message, setMessage] =useState("");
   const [logState, setLogState ]= useState(false);
   const[formState, setFormState]= useState ({
     name: '',
     password: '',
-    
   });
   
    const {name, password}=formState;
 
    const handleImputChange = ({target}) =>{
-
     setFormState({
       ...formState,
       [target.name]: target.value
     });
   }
 
-    useEffect(()=>{
-    if(name=="david" && password=="1234"){
-      setIsLogin(true)
-            
-    }else if(logState){
+  const handleClick = () => {
+    if(name === "" || password === ""){
+      setMessage("Todos los campos son obligatorios")
+      setLogState(true);
+      return;
+    }
 
-      console.log("No es posible loguear")
-      setLogState(false)
+    if(name === "david" && password === "1234"){
+      setIsLogin(true);
+    }else{
       setMessage("Usuario o contraseña incorrecto")
       setTimeout(() => {
           setMessage("")
       }, 2000);
     }
-  },  [logState]);
-  
-
-  useEffect(()=>{
-    if((logState) && (name=="" || password=="")){
-      setMessage("Todos los campos son obligatorios")
-      
-    }
-  },[logState]);
+  }
 
   useEffect(()=>{
     if (isLogin){
       localStorage.setItem("isLogin", isLogin)
       navigate("/home") 
-      console.log(isLogin)
     }
   },[isLogin]);
 
-
-  //Local Storage
-  // Obtener desde local storage: 
-   //localStorage.getItem("isLogin");
-
-
   return(
     <>
-    <div className='myBody' >
-      {/* <CssBaseline  / > */}
-      
+    <div className='myBody' >      
       <Container maxWidth="sm">
        <div className='header'>
         <AccountCircleOutlinedIcon className='icon' fontSize="large" />
         <h1 className='head'>Iniciar sesión</h1>
       </div>
-      
-        
         <Box className='box' sx={{ bgcolor: '#DFDBE2', height: '50vh' }} >
           <Stack spacing={3}>
            <TextField 
-
-            error={name === ""}
+            error={logState ? name === "" : false}
             id="outlined-basic" 
             name="name"
             label="Usuario" 
@@ -96,13 +73,11 @@ const Login = () => {
             value={name}
             onChange={handleImputChange}
             autoComplete="off"
-            helperText={name === "" ? 'Ingrese su Usuario!' : ' '}
-          
+            helperText={logState && name === "" ? 'Ingrese su Usuario!' : ''}
             />
 
-           <TextField 
-            
-            error={password === ""}
+           <TextField  
+            error={logState ? password === "" : false}
             id="outlined-basic" 
             name='password'
             label="Contraseña" 
@@ -112,22 +87,12 @@ const Login = () => {
             value={password}
             onChange={handleImputChange}
             autoComplete="off"
-            helperText={password === "" ? 'Ingrese su constraseña!' : ' '}
-            
+            helperText={logState && password === "" ? 'Ingrese su constraseña!' : ' '}
             />
-           <Button 
-            onClick={()=>setLogState(true)}
-            
-              variant="contained"
-
-            >Ingresar</Button>
-
+           <Button onClick={handleClick}variant="contained">Ingresar</Button>
             <h2 className='incorrect'>{message}</h2>
-
-          
         </Stack>
         </Box>
-
       </Container> 
       </div> 
     </>
