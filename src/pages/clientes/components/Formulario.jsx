dc8008542f474effe67f8dcbc75d81c08eeb42e2
David Henao <daves.hb@hotmail.com>
2022-03-19 17:57:24 -0500

    Clientes LISTO

--- src/pages/clientes/components/Formulario.jsx
+++ src/pages/clientes/components/Formulario.jsx
@@ -5,7 +5,13 @@ import Button from "@mui/material/Button";
 import BasicSelect from "./BasicSelect";
 import "../styles.scss";
 
-export default function Formulario({select, handleSubmit, handleSubmit2, form, age, setAge,
+export default function Formulario({
+  select,
+  handleSubmit,
+  handleSubmit2,
+  form,
+  age,
+  setAge,
 }) {
   const [data, setData] = React.useState({
     documentNumber: form ? " " : select.documentNumber,
@@ -15,79 +21,73 @@ export default function Formulario({select, handleSubmit, handleSubmit2, form, a
     id: form ? " " : select.id,
     phone: form ? " " : select.phone,
   });
-
-  console.log(data.documentType);
-
   return (
     <>
-    
-    <Box
-    className="doc"
-      component="form"
-      sx={{
-        // "& > :not(style)": { m: 1, width: "25ch" },
-      }}
-      noValidate
-      autoComplete="off"
-    >
-
-      
-      <TextField
-        name="documentNumber"
-        value={data.documentNumber}
-        onChange={(event) =>
-          setData({ ...data, documentNumber: event.target.value })
+      <Box
+        className="doc"
+        component="form"
+        sx={
+          {
+            // "& > :not(style)": { m: 1, width: "25ch" },
+          }
         }
-        id="outlined-basic"
-        label="Numero de documento"
-        variant="outlined"
-        InputLabelProps={{
-          shrink: true,
-        }}
-      />
+        noValidate
+        autoComplete="off"
+      >
+        <TextField
+          name="documentNumber"
+          value={data.documentNumber}
+          onChange={(event) =>
+            setData({ ...data, documentNumber: event.target.value })
+          }
+          id="outlined-basic"
+          label="Numero de documento"
+          variant="outlined"
+          InputLabelProps={{
+            shrink: true,
+          }}
+        />
 
+        <BasicSelect age={age} setAge={setAge} data={data} setData={setData} />
 
-      
-      <BasicSelect age={age} setAge={setAge} data={data} setData={setData} />
+        <TextField
+          name="fullName"
+          value={data.fullName}
+          onChange={(event) =>
+            setData({ ...data, fullName: event.target.value })
+          }
+          id="outlined-basic"
+          label="Nombre"
+          variant="outlined"
+          InputLabelProps={{
+            shrink: true,
+          }}
+        />
 
-      <TextField
-        name="fullName"
-        value={data.fullName}
-        onChange={(event) => setData({ ...data, fullName: event.target.value })}
-        id="outlined-basic"
-        label="Nombre"
-        variant="outlined"
-        InputLabelProps={{
-          shrink: true,
-        }}
-      />
+        <TextField
+          name="email"
+          value={data.email}
+          onChange={(event) => setData({ ...data, email: event.target.value })}
+          id="outlined-basic"
+          label="correo electrónico"
+          variant="outlined"
+          InputLabelProps={{
+            shrink: true,
+          }}
+        />
 
-      <TextField
-        name="email"
-        value={data.email}
-        onChange={(event) => setData({ ...data, email: event.target.value })}
-        id="outlined-basic"
-        label="correo electrónico"
-        variant="outlined"
-        InputLabelProps={{
-          shrink: true,
-        }}
-      />
-
-
-      <TextField
-        name="phone"
-        value={data.phone}
-        onChange={(event) => setData({ ...data, phone: event.target.value })}
-        id="outlined-basic"
-        label="Telefono"
-        variant="outlined"
-        InputLabelProps={{
-          shrink: true,
-        }}
-      />
-
-    </Box>
+        <TextField
+          name="phone"
+          value={data.phone}
+          onChange={(event) => setData({ ...data, phone: event.target.value })}
+          id="outlined-basic"
+          label="Telefono"
+          variant="outlined"
+          InputLabelProps={{
+            shrink: true,
+          }}
+        />
+      </Box>
       <Button
         className="botonModal"
         variant="contained"
@@ -96,7 +96,6 @@ export default function Formulario({select, handleSubmit, handleSubmit2, form, a
       >
         {form ? "Agregar" : "Actualizar"}
       </Button>
-    
     </>
   );
 }
